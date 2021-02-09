61be7c24dad1740fb82064e0f30490cec2d395f5
Duc Nguyen Dang <ducnd@mpos.vn>
2021-02-09 09:30:49 +0700

    Commit Lần 2

---
+++ src/main/java/com/ndangduc/bn/startspringboot/StartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn.startspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndangduc/bn/startspringboot/StartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn.startspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
